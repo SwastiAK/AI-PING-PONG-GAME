967e20ab2d2c6ba8f12e7f99031fb06e4af7d6af
SwastiAK <85284257+SwastiAK@users.noreply.github.com>
2022-04-27 13:07:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+/*created by prashant shukla */
+
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -20,15 +22,19 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  var canvas = createCanvas(700,600);
   canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
 
 function draw(){
-
  background(0); 
-
+ image(video,0,0,700,600);
+ 
  fill("black");
  stroke("black");
  rect(680,0,20,700);
@@ -160,4 +166,8 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+function modelLoaded(){
+  console.log("model_loaded");
 }
